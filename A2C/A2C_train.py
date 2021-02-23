543125101d4351371a6d37af94ee333e033a69ff
nltanhoang99 <77202625+nltanhoang99@users.noreply.github.com>
2021-02-23 12:20:54 +0100

    Update A2C_train.py

--- A2C/A2C_train.py
+++ A2C/A2C_train.py
@@ -11,27 +11,27 @@ def getState(data, start, window):
 
 
 def evalTrade(a, tr, next_step):
-    if tr[0] == 1:  # LONG
+    if tr[0] == 1 or tr[0] == 2 or tr[0] == 3:  # LONG
         if next_step[-1][1] >= tr[3]:  # TP
             r = 3
             a.in_trade = False
-            return [1, r]
+            return [tr[0], r]
 
         elif next_step[-1][3] <= tr[2]:  # SL
             r = -1
             a.in_trade = False
-            return [1, r]
+            return [tr[0], r]
 
-    elif tr[0] == 2:  # SHORT
+    elif tr[0] == 4 or tr[0] == 5 or tr[0] == 6:  # SHORT
         if next_step[-1][3] <= tr[3]:  # TP
             r = 3
             a.in_trade = False
-            return [2, r]
+            return [tr[0], r]
 
         elif next_step[-1][1] >= tr[2]:  # SL
             r = -1
             a.in_trade = False
-            return [2, r]
+            return [tr[0], r]
     return [0, 0]
 
 
@@ -45,11 +45,31 @@ def takeTrade(action, ohlc):
 
     # (long/short, entry, SL, TP)
     if action == 1:
+        sl = (unscaled_close - 0.0002 - data_min) / (data_max - data_min)
+        tp = (unscaled_close + 0.0006 - data_min) / (data_max - data_min)
+        return action, close, sl, tp
+
+    elif action == 2:
+        sl = (unscaled_close - 0.0005 - data_min) / (data_max - data_min)
+        tp = (unscaled_close + 0.0015 - data_min) / (data_max - data_min)
+        return action, close, sl, tp
+
+    elif action == 3:
         sl = (unscaled_close - 0.0010 - data_min) / (data_max - data_min)
         tp = (unscaled_close + 0.0030 - data_min) / (data_max - data_min)
         return action, close, sl, tp
 
-    elif action == 2:
+    elif action == 4:
+        sl = (unscaled_close + 0.0002 - data_min) / (data_max - data_min)
+        tp = (unscaled_close - 0.0006 - data_min) / (data_max - data_min)
+        return action, close, sl, tp
+
+    elif action == 5:
+        sl = (unscaled_close + 0.0005 - data_min) / (data_max - data_min)
+        tp = (unscaled_close - 0.0015 - data_min) / (data_max - data_min)
+        return action, close, sl, tp
+
+    elif action == 6:
         sl = (unscaled_close + 0.0010 - data_min) / (data_max - data_min)
         tp = (unscaled_close - 0.0030 - data_min) / (data_max - data_min)
         return action, close, sl, tp
@@ -64,23 +84,25 @@ def evalPass(current, next):
     return 0
 
 
-WINDOW_SIZE = 30
+WINDOW_SIZE = 90
 EPISODE_LENGTH = 2500 - WINDOW_SIZE
-EPISODE_NUM = 6500
+EPISODE_NUM = 10000
 
 agent = ActorCritic(WINDOW_SIZE)
+agent = ActorCritic(WINDOW_SIZE, is_eval=True, actor_name="ep-2800-actor.h5", critic_name="ep-2800-critic.h5")
 win, loss = 0, 0
 ps, lg, st = 0, 0, 0
-chkpnt = 0
+
 
 loop = tqdm(total=EPISODE_NUM, position=0, leave=False)
 for episode in range(EPISODE_NUM):
+    ACTIONS = [0, 0, 0, 0, 0, 0, 0]
     loop.set_description('Training ...'.format(episode))
     loop.update(1)
 
     # Randomize trading environment every episode
     chunk = random.randint(0, 79)
-    print(f"--- Starting chunk {chunk} ---")
+    print(f"Starting chunk {chunk}")
     srow = int(chunk * 2500 + 1)
     df = pd.read_csv('EU_M5_2017_2020.csv',
                      index_col='Local time',
@@ -110,13 +132,7 @@ for episode in range(EPISODE_NUM):
         if not agent.in_trade:
             action = agent.act(state)
 
-            if action == 1:
-                trade = takeTrade(action, state[-1])
-                entry_state = state
-                entry_t = t
-                agent.in_trade = True
-
-            elif action == 2:
+            if action != 0:
                 trade = takeTrade(action, state[-1])
                 entry_state = state
                 entry_t = t
@@ -133,32 +149,34 @@ for episode in range(EPISODE_NUM):
                 else:
                     loss += 1
                     ret -= 1
-                if result[0] == 1:
-                    lg += 1
-                else:
-                    st += 1
+                ACTIONS[result[0]] += 1
                 reward = result[1]
                 agent.remember(entry_state, action, reward)
 
         # Passing
         else:
             reward = evalPass(state, next_state)
-            ps += 1
+            ACTIONS[0] += 1
             agent.remember(state, action, reward)
 
         if t == EPISODE_LENGTH - 1 or ret <= -100:
 
-            print(f"--- Cumulative return Episode {episode} (chunk {chunk}): {ret} ---")
-            print(f"--- Wins: {win}     Losses: {loss} ---")
+            print("--------------------------------------------------------------------")
+            print(f"Cumulative return Episode {episode} (chunk {chunk}): {ret}")
+            print(f"Wins: {win}     Losses: {loss}")
             if win + loss > 0:
-                print(f"--- Winrate: {win / (win + loss)} ---")
-            print(f"--- Passes: {ps}    Longs: {lg}     Shorts: {st} ---")
-
+                print(f"Winrate: {win / (win + loss)}")
+            print(f"Passes: {ACTIONS[0]}")
+            print(f"L2: {ACTIONS[1]}     L5: {ACTIONS[2]}      L10:  {ACTIONS[3]}")
+            print(f"S2: {ACTIONS[4]}     S5: {ACTIONS[5]}      S10:  {ACTIONS[6]}")
+            print("--------------------------------------------------------------------")
             agent.train()
+
             break
 
         state = next_state
 
     if episode % 25 == 0:
-        agent.actor.save(f"ep-{episode}-actor.h5")
-        agent.critic.save(f"ep-{episode}-critic.h5")
+        print("SAVING MODELS")
+        agent.actor.save(f"ep-{episode + 2800}-actor.h5")
+        agent.critic.save(f"ep-{episode + 2800}-critic.h5")
